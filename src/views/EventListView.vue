bd36dfaf7c2e421206b8aecf9418287cc77fa661
mjwfilippo <mjwfilippo@gmail.com>
2023-05-22 15:02:30 +0200

    added pagination and DB upgrade

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,18 +1,32 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed, watchEffect } from "vue";
 import EventService from "@/services/EventService.js";
 import EventCard from "@/components/EventCard.vue";
 
+const props = defineProps(["page"]);
+
 const events = ref(null);
+const totalEvents = ref(0);
+
+const page = computed(() => props.page);
+
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / 2);
+  return page.value < totalPages;
+});
 
 onMounted(() => {
-  EventService.getEvents()
-    .then(response => {
-      events.value = response.data;
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  watchEffect(() => {
+    events.value = null;
+    EventService.getEvents(2, props.page)
+      .then(response => {
+        events.value = response.data;
+        totalEvents.value = response.headers["x-total-count"];
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 });
 </script>
 
@@ -20,6 +34,20 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <RouterLink
+      :to="{ name: 'event-list', query: { page: page - 1 } }"
+      rel="prev"
+      v-if="page != 1"
+      >Prev Page</RouterLink
+    >
+
+    <RouterLink
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+      v-if="hasNextPage"
+      >Next Page</RouterLink
+    >
   </div>
 </template>
 
