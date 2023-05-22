b8b8c5565742b247021ee89232c62572bcd20686
mjwfilippo <mjwfilippo@gmail.com>
2023-05-22 11:45:04 +0200

    all lessons up to fake db json server

---
+++ src/views/EventListView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import EventCard from "@/components/EventCard.vue";
+
+const events = ref(null);
+
+onMounted(() => {
+  axios
+    .get(
+      "https://my-json-server.typicode.com/mjwfilippo/real-world-vue-3-new-syntax/events"
+    )
+    .then(response => {
+      console.log("events:", response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
